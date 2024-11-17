c12de9ce37f2fcdf19ea1df0db7c1b3ded70354d
Wahyu Supriyadi <wahyu@istw.co.id>
2024-11-18 05:31:56 +0700

    add counter

--- laravel/durusul-lughoh-kalimat/routes/web.php
+++ laravel/durusul-lughoh-kalimat/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Counter;
+use App\Livewire\CreateCustomer;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/counter', Counter::class);
+Route::get('/create', CreateCustomer::class);
